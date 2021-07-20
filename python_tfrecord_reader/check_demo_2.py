dc667dd5e807151c699c2cd5419b69d1afb87bca
joonhwan <joonhwan.lee@gmail.com>
2021-07-20 23:40:59 +0900

    updated py demo2

--- python_tfrecord_reader/check_demo_2.py
+++ python_tfrecord_reader/check_demo_2.py
@@ -23,9 +23,21 @@ def _parse_function(example_proto):
 ds = tf.data.TFRecordDataset(tfrecord_path)
 ds = ds.map(_parse_function)
 
+read_check_dir = Path("./.datasets/marble")
 for example in ds:
-    name = example['name'].numpy() # bytes
-    imageFile = example['imageFile'].numpy()  # bytes
+    name = example['name'].numpy() # --> bytes
+    name = name.decode('utf-8') #  --> string(utf-8)
+    name = Path(name).name
+    imageFile = example['imageFile']  # Tensor
+    imageFileBytes = imageFile.numpy()
     label = example['label'].numpy() # bytes 
+    label = label.decode('utf-8') # -> string(utf-8)
     # print(type(name), type(imageFile), type(label))
-    print(f'{name} : {len(imageFile)} bytes, label={label}')
+    print(f'{name} : {len(imageFileBytes)} bytes, label={label}')
+    imageOutPath = str(read_check_dir / label / name)
+    print(imageOutPath)
+    tf.io.write_file(imageOutPath, imageFile)
+
+
+#%%
+ 
